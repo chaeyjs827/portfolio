e73b1507ff603979ce0d9e89b651df70934009fd
JUN <chaeyjs827@gmail.com>
2019-12-26 23:16:53 +0800

    2019.12.26 commit

--- src/com/pf/config/WebConfig.java
+++ src/com/pf/config/WebConfig.java
@@ -2,10 +2,13 @@ package com.pf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
@@ -20,16 +23,17 @@ public class WebConfig implements WebMvcConfigurer{
 		return tilesConfigurer;
 	}
 	
-//	@Bean
-//	public MappingJackson2JsonView jsonView() {
-//		MappingJackson2JsonView mj2jv = new MappingJackson2JsonView();
-//		mj2jv.setContentType("application/json;charset=UTF-8");
-//		return mj2jv;
-//	}
-//    @Bean
-//    MappingJackson2JsonView jsonView(){
-//        return new MappingJackson2JsonView();
-//    }
+	@Bean
+	public MappingJackson2JsonView jsonView() {
+		MappingJackson2JsonView mj2jv = new MappingJackson2JsonView();
+		mj2jv.setContentType("application/json;charset=UTF-8");
+		return mj2jv;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		return new BeanNameViewResolver();
+	}
 	
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
--- src/com/pf/controller/MemberController.java
+++ src/com/pf/controller/MemberController.java
@@ -2,16 +2,22 @@ package com.pf.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController extends Member {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public ModelAndView LoginJSON(HttpServletRequest request) throws Exception {
-//		ModelAndView mav = new ModelAndView("jsonView");
-		ModelAndView mav = new ModelAndView();
+		ModelAndView mav = new ModelAndView("jsonView");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("test", "testValue");
+		jsonObject.put("haha", "YEAH");
+		mav.addObject("jsonObject", jsonObject);
+//		ModelAndView mav = new ModelAndView();
 		return mav;
 	}
 	
