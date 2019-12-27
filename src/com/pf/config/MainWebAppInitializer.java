05e649d40bf78df5c569226881a376e4b8906543
JUN <JUN@DESKTOP-HV1IQG5>
2019-12-27 21:18:14 +0800

    2019.12.27 commit

--- src/com/pf/config/MainWebAppInitializer.java
+++ src/com/pf/config/MainWebAppInitializer.java
@@ -16,7 +16,10 @@ public class MainWebAppInitializer implements WebApplicationInitializer{
 	public void onStartup(final ServletContext sc) throws ServletException {
 		AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
 		root.scan("com.pf");
+		
+		// 스프링 ioc컨테이너를 , application-context를 servlet-application의 생명 주기에 맞춰 바인딩해줌
 		sc.addListener(new ContextLoaderListener(root));
+		
 		ServletRegistration.Dynamic appServlet = sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
 		appServlet.setLoadOnStartup(1);
 		appServlet.addMapping("/");
