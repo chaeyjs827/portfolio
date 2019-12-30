6680e50fe5a09b7e06348e41eafa5a9efb81194e
JUN <JUN@DESKTOP-HV1IQG5>
2019-12-30 20:41:56 +0800

    2019.12.30 commit

--- src/com/pf/controller/MainController.java
+++ src/com/pf/controller/MainController.java
@@ -4,6 +4,9 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,8 @@ import com.pf.sao.MemberSao;
 @Controller
 public class MainController extends Main {
 
+	final static Logger logger = LoggerFactory.getLogger(MainController.class);
+	
 	@Autowired
 	MemberSao memberSao;
 	
@@ -30,6 +35,11 @@ public class MainController extends Main {
 		Locale.setDefault(new Locale("en", "EN"));
 		System.out.println("clientLocale : " + locale);
 		System.out.println("sessionLocale : " + localeResolver.resolveLocale(request));
+		
+		logger.info("logger info");
+		logger.warn("logger warn");
+		logger.error("logger error");
+		logger.debug("logger debug");
 		
 //		memberSao.testSao();
 		return mav;
