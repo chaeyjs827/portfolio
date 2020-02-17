8b4c5f9ea7d804f6fd0d8a60d896ce85a440c4c5
JUN <chaeyjs827@gmail.com>
2020-02-17 23:09:09 +0800

    2020.02.17 commit

--- src/main/java/com/pf/controller/MainController.java
+++ src/main/java/com/pf/controller/MainController.java
@@ -1,18 +1,20 @@
 package com.pf.controller;
 
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pf.dao.ContentDao;
 import com.pf.sao.MemberSao;
 
 @Controller
@@ -29,10 +31,14 @@ public class MainController extends Main {
 	@Autowired
 	LocaleResolver localeResolver;
 	
+	@Autowired
+	ContentDao contentDao;
 	
 	@Override
 	public ModelAndView indexMain(HttpServletRequest request, Locale locale) {
 		ModelAndView mav = new ModelAndView("legacyIndexMain");
+		
+		List<Map<String, Object>> portfolioList = contentDao.getPortfolioList();
 		
 		logger.info("[logger info]");
 		logger.warn("[logger warn]");
--- src/main/java/com/pf/dao/ContentDao.java
+++ src/main/java/com/pf/dao/ContentDao.java
@@ -1,5 +1,11 @@
 package com.pf.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
@@ -9,5 +15,14 @@ import com.pf.config.ProjectConstants;
 @Transactional
 @Repository
 public class ContentDao extends ProjectConstants {
-
+	
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public List<Map<String,Object>> getPortfolioList() {
+		List<Map<String,Object>> portfolioList = new ArrayList<>();
+//		portfolioList.
+		return portfolioList;
+	}
+	
 }
